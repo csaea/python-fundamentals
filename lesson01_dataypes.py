205d7d056b04e4e4dd62ba62f4439102aaf07b15
Computer Science and Applied Engineering Academy <mharrell@hcvsd.org>
2025-09-24 16:17:59 +0000

    creates datatypes lesson

---
+++ lesson01_dataypes.py
@@ -0,0 +1,36 @@
+# This is a comment
+#DATA TYPES
+
+# 1. STRINGS:
+print("Hello world")
+print("Once upon a time and a very good time it was there was a moocow coming down along the road and this moocow that was down along the road met a nicens little boy named baby tuckoo")
+print(" ")
+print("Once upon a time \nand a very good time it was \nthere was a moocow")
+print("Name: \tBilly \nAge: \t14 \nGrade: \t9")
+
+# INTEGERS
+
+print(42)
+print(-23)
+print(5 + 5 - 11)
+
+#FLOAT 
+print(4.0)
+print(2.0 + 4.5)
+print(2.0 + 4) #returns a float
+
+#BOOLEAN 
+print(True) # 1
+print(False) # 0
+print(1 == 1)
+print(1 != 1)
+print("Hello" == "hello")
+print(5 + 5 == 11)
+
+#How do I know what data type I'm working with? 
+print("\nDATA TYPE LOOKUP:\n") 
+
+print(type("To see this message, wrap me in a print statement"))
+print(type(3))
+print(type(3.14))
+print(type(1 == 1))
