26a780f2b0fd566f58a20dfebd1657d8dbfb314c
Computer Science and Applied Engineering Academy <mharrell@hcvsd.org>
2025-09-25 17:26:05 +0000

    stage commit

---
+++ lesson02_variables.py
@@ -0,0 +1,72 @@
+#CONCEPTS VARIABLES
+
+taco = "Hell world"
+
+password = "G00seberryPie5"
+email = "myharrell@nhvweb.net"
+print("Password: ", password)
+print("Email: ", email)
+
+temperature = 72.5
+print(type(temperature))
+price = 3.99
+
+enabled = False
+is_complete = True
+print(is_complete)
+enabled = True
+print(enabled)
+
+# for math problems:
+
+x = 3.14
+y = 7
+print(x + y)
+
+# variables are flexible. you create or update another variable, like so:
+count = 10 
+print(count)
+count_down = count - 1
+print(count_down)
+count = count_down
+print(count)
+count_down = count - 1
+print(count_down)
+count = count_down
+print(count)
+
+# Challenge 1: Rename Variables  
+# Change the variable names x, y, z below to more descriptive names. 
+
+x = "Radia Perlman"
+y = 34
+z = "Networking Engineer"
+
+# Challenge 2: Update Variables  
+# Create a variable called 'count' with a value of 10.  
+# Increase it by 5 and print the result.  
+# Use two variables total. 
+
+# Challenge 3: Swap Variables  
+# Given variables x = 4 and y = "hello".  
+# Swap the values so that x = "hello" and y = 4. 
+# Use a temporary variable.  
+# Hint: You will need to create one new variable. 
+
+x = 4
+y = "hello"
+temp = y
+y = x
+x = temp
+print("x = ", x)
+print("y = ", y)
+
+# Challenge 4: Boolean Practice  
+# Create a variable named 'is_raining' set to False.  
+# On the next line, update the variable to True. 
+# Print "Is it raining?" followed by the boolean. 
+
+is_raining = False
+print("Is it raining?:", is_raining)
+is_raining = True
+print("Is it raining?:", is_raining)
